63c5c0ffa3b39eb7af33b6da6e18c1564877b0ba
Victor Vialard <victor.vialard@mines-paristech.fr>
2021-12-07 12:58:21 +0100

    AOC - 3

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -13,7 +13,7 @@ fn main() {
         let mut zeros = 0;
 
         for line in content.lines() {
-            let num = line.get(i..i + 1).unwrap().parse::<u32>().unwrap();
+            let num = line.get(i..i + 1).unwrap().parse::<u8>().unwrap();
             match num {
                 0 => zeros = zeros + 1,
                 1 => ones = ones + 1,
@@ -30,5 +30,110 @@ fn main() {
         };
     }
 
-    println!("{}", gamma * epsilon)
+    println!("{}", gamma * epsilon);
+
+    let mut lines: Vec<usize> = (0..content.lines().count()).collect();
+    let mut i = 0;
+
+    while lines.len() > 1 {
+        let mut ones = 0;
+        let mut zeros = 0;
+
+        for line in lines.clone() {
+            let num = content
+                .lines()
+                .nth(line)
+                .unwrap()
+                .chars()
+                .nth(i)
+                .unwrap()
+                .to_digit(10)
+                .unwrap();
+
+            match num {
+                0 => zeros = zeros + 1,
+                1 => ones = ones + 1,
+                _ => panic!("Unvalid bit"),
+            }
+        }
+
+        lines = lines
+            .into_iter()
+            .filter(|x| {
+                content
+                    .lines()
+                    .nth(*x)
+                    .unwrap()
+                    .chars()
+                    .nth(i)
+                    .unwrap()
+                    .to_digit(10)
+                    .unwrap()
+                    == (if ones >= zeros { 1 } else { 0 })
+            })
+            .collect();
+
+        i = i + 1;
+    }
+    let oxy = content
+        .lines()
+        .nth(*lines.first().unwrap())
+        .unwrap()
+        .chars()
+        .map(|x| x.to_digit(10).unwrap())
+        .fold(0, |result, c| result * 2 + c);
+
+    let mut lines: Vec<usize> = (0..content.lines().count()).collect();
+    let mut i = 0;
+
+    while lines.len() > 1 {
+        let mut ones = 0;
+        let mut zeros = 0;
+
+        for line in lines.clone() {
+            let num = content
+                .lines()
+                .nth(line)
+                .unwrap()
+                .get(i..i + 1)
+                .unwrap()
+                .parse::<u8>()
+                .unwrap();
+
+            match num {
+                0 => zeros = zeros + 1,
+                1 => ones = ones + 1,
+                _ => panic!("Unvalid bit"),
+            }
+        }
+
+        lines = lines
+            .into_iter()
+            .filter(|x| {
+                content
+                    .lines()
+                    .nth(*x)
+                    .unwrap()
+                    .get(i..i + 1)
+                    .unwrap()
+                    .parse::<u8>()
+                    .unwrap()
+                    == (if zeros <= ones { 0 } else { 1 })
+            })
+            .collect();
+
+        i = i + 1;
+    }
+
+    let co = content
+        .lines()
+        .nth(*lines.first().unwrap())
+        .unwrap()
+        .chars()
+        .map(|x| x.to_digit(10).unwrap())
+        .fold(0, |result, c| result * 2 + c);
+
+    println!("{}", co);
+    println!("{}", oxy);
+    println!("{}", oxy * co)
 }
