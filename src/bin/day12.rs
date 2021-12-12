53981205240e8f4fa451f991f764aa843d954b40
Victor Vialard <victor.vialard@mines-paristech.fr>
2021-12-12 22:48:11 +0100

    AOC 12

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -5,9 +5,11 @@ const FILENAME: &str = "input/day12.txt";
 
 fn count_paths<'a>(
     graph: &HashMap<&str, HashSet<&'a str>>,
-    visited: &mut HashSet<&'a str>,
+    visited: &mut HashMap<&'a str, u32>,
     current: &str,
     end: &str,
+    start: &str,
+    single: bool,
 ) -> u32 {
     graph
         .get(&current)
@@ -15,15 +17,26 @@ fn count_paths<'a>(
         .iter()
         .fold(0, |acc, next| match next {
             next if end.eq(*next) => acc + 1,
-            next if visited.contains(next)
-                && !next.chars().next().unwrap().is_ascii_uppercase() =>
+            next if start.eq(*next) => acc,
+            next if *visited.get(next).unwrap_or(&0) >= 1
+                && !next.chars().next().unwrap().is_ascii_uppercase()
+                && single =>
             {
                 acc
             }
             _ => {
-                visited.insert(next);
-                let count = count_paths(graph, visited, next, end);
-                visited.remove(next);
+                *visited.entry(next).or_insert(0) += 1;
+                let count = count_paths(
+                    graph,
+                    visited,
+                    next,
+                    end,
+                    start,
+                    (*visited.get(next).unwrap() >= 2
+                        && !next.chars().next().unwrap().is_ascii_uppercase())
+                        || single,
+                );
+                *visited.get_mut(next).unwrap() -= 1;
                 acc + count
             }
         })
@@ -41,7 +54,10 @@ fn main() {
             graph.entry(b).or_insert(HashSet::new()).insert(a);
         });
 
-    let mut visited = HashSet::new();
-    visited.insert("start");
-    println!("{}", count_paths(&graph, &mut visited, "start", "end"));
+    let mut visited = HashMap::new();
+    visited.insert("start", 1);
+    println!(
+        "{}",
+        count_paths(&graph, &mut visited, "start", "end", "start", false)
+    );
 }
